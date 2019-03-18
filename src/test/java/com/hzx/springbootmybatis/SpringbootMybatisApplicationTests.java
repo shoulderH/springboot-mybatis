ccfffb8c6c7e6199b23662da1bd62d376cbed6e0
shoulderH <951088543@qq.com>
2019-03-18 15:41:19 +0800

    changeTest

--- src/test/java/com/hzx/springbootmybatis/SpringbootMybatisApplicationTests.java
+++ src/test/java/com/hzx/springbootmybatis/SpringbootMybatisApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SpringbootMybatisApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void getContents() {
 
 
 	}
